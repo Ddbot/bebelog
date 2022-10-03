4fe5664eafc13e96776bac2bce4a5797b0389435
Ddbot <rakotoniaina.andry@gmail.com>
2022-10-03 22:31:49 +0200

    Changé design de EventCard

--- src/screens/eventsListScreen/EventCard.tsx
+++ src/screens/eventsListScreen/EventCard.tsx
@@ -6,35 +6,64 @@ import { supabase } from '../../supabase/client';
 
 const Card = styled.div`
     width: 100%;
-    height: 100%;
-
-    display: flex;
-    flex-flow: row nowrap;
-    justify-content: space-between;
-    align-items: flex-start;
-
-    border: 1px solid red;
-
+    height:calc(100% - 7rem);
 `;
 const Content = styled.div`
     width: 100%;
-    height: 3rem;
+    height: 100%;
 
-    padding: 2rem 0.75rem;
-
-
-
-    display: flex;
-    flex-flow: row nowrap;
-    justify-content: space-between;
-    align-items: center;
-
-    background: papayawhip;
+    display: grid; 
+    grid-auto-rows: 1fr; 
+    grid-template-columns: 1fr 1fr 1fr; 
+    grid-template-rows: 1fr 1fr 1fr; 
+    place-items: stretch;
 `;
 
 const Icon = styled.div`
-    scale: 1.2;
-    
+    grid-column: 1 / span 3;
+    grid-row: 1 / span 1;
+    scale: 4;
+    transform-origin: top center;
+    margin-top: 24px;
+`;
+
+const Values = styled.div`
+    grid-column: 1 / span 3;
+    grid-row: 2 / span 1;
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: space-evenly;
+    justify-content: center;
+    & > div {
+        display: flex;
+        flex-flow: row nowrap;
+        align-items: center;
+        justify-content: center;
+            width: 100%;
+  
+    }
+    `;
+
+const ButtonContainer = styled.div`
+    place-self: end center;
+    width: 100%;
+
+    grid-column: 1 / span 3;
+    grid-row: 3 / span 1;
+
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-evenly;
+
+    padding-bottom: 24px;
+
+    & > button {
+        margin: 4px;
+        font-size: 1.4rem;
+        text-align: center;
+
+        scale: 1.5;
+    }
 `;
 
 const EventCard = () => { 
@@ -48,13 +77,37 @@ const EventCard = () => {
 
         if (error) console.error(error);
         navigate('/events_list')
-    };    
+    }; 
+    
+    async function editEvent(event: React.MouseEvent<Element>) {
+        console.log('ON VA DANS UNE PAGE FORM pour changer les infos de lEVENT');        
+    };     
+
+    async function confirmChange(event: React.MouseEvent<Element>) {
+        console.log('ici on envoie a superbase lupdate');        
+    };     
 
     return !['dodo','nourriture'].includes(value!.type) ? <Card>
-        <Content><Icon>{icons[value!.type]}</Icon><span>{ value!.start}</span><span><button id={value.id} onClick={deleteEvent}>🗑️</button></span>
-    </Content></Card> : <Card><Content>
-            <span>{icons[value!.type]} { (value!.end! - value!.start)/1000 }</span><span><button id={value.id} onClick={deleteEvent}>🗑️</button></span>
-    </Content></Card>
+        <Content>
+            <Icon>{icons[value!.type]}</Icon>
+            <Values><div>{value!.start}</div></Values>
+            <ButtonContainer>
+                <button id={value.id} onClick={editEvent}>🖊️</button>
+                <button id={value.id} onClick={deleteEvent}>🗑️</button>
+                <button id={value.id} onClick={confirmChange}>✔️</button>
+            </ButtonContainer>
+        </Content>
+    </Card> : <Card>
+        <Content>
+            <Icon>{icons[value!.type]}</Icon>
+                <Values><div>{(value!.end! - value!.start) / 1000}</div></Values>
+                <ButtonContainer>
+                    <button id={value.id} onClick={editEvent}>🖊️</button>
+                    <button id={value.id} onClick={deleteEvent}>🗑️</button>
+                    <button id={value.id} onClick={confirmChange}>✔️</button>
+                </ButtonContainer>
+            </Content>
+        </Card>
     
 };
 
