abb142edded0c99d2b9635e60dbf8b6724cf687d
Ddbot <rakotoniaina.andry@gmail.com>
2022-10-03 14:08:32 +0200

    Mis les icons ds leur propre module

---
+++ src/assets/icons.tsx
@@ -0,0 +1,13 @@
+import Lavage from './Lavage';
+import Change from './Change';
+import Dodo from './Dodo';
+import Medicament from './Medicament';
+import Nourriture from './Nourriture';
+
+export const icons: any = {
+    lavage: <Lavage />,
+    change: <Change />,
+    medicament:<Medicament />,
+    dodo:<Dodo />,
+    nourriture: <Nourriture />
+};
