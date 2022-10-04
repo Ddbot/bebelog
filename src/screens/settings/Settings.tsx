14b3cc5b736c1ca075b9a366950c3ccb1f610273
Ddbot <rakotoniaina.andry@gmail.com>
2022-10-04 17:14:51 +0200

    Cree state settings

--- src/screens/settings/Settings.tsx
+++ src/screens/settings/Settings.tsx
@@ -1,4 +1,4 @@
-import {  SetStateAction, useState } from "react";
+import React, {  SetStateAction, useState } from "react";
 import { Container, H1, Ul, Li } from './styled-components';
 import { NourritureType } from "../../models/Event";
 import dayjs, { Dayjs } from 'dayjs';
@@ -21,10 +21,17 @@ const SettingsPage = (props: Props) => {
         nourriture: 'sein',
         objectif: 30
     });
+
+    function handleChange(event:React.ChangeEvent<HTMLInputElement>) {
+        const { currentTarget } = event;
+        console.log('Input name => ', currentTarget.name);
+        
+    };
+
     return <Container>
         <H1>Reglages</H1>
         <Ul>
-            <Li>name of baby</Li>
+            <Li><input name="name" onChange={handleChange} placeholder="coucou" /></Li>
             <Li>Date de naissance</Li>
             <Li>nourriture: sein ou biberon ?</Li>
             <Li>Si biberon = objectifs de nourriture (ml)</Li>
