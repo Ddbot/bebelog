b0d109c69f867ca49176dfbc3a7bd5435a0ec6aa
Ddbot <rakotoniaina.andry@gmail.com>
2022-10-04 20:21:21 +0200

    Added BackArrow &Pen svg icons

---
+++ src/assets/BackArrow.tsx
@@ -0,0 +1,18 @@
+const BackArrow = () => (
+  <svg
+    viewBox="0 0 24 24"
+    width={24}
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    style={{
+      fillRule: "evenodd",
+      clipRule: "evenodd",
+      strokeLinejoin: "round",
+      strokeMiterlimit: 2,
+    }}
+  >
+    <path d="M12 16.2V24L0 12 12 0v7.8h12v8.4H12Z" />
+  </svg>
+);
+
+export default BackArrow;
