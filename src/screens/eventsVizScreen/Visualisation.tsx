60ce535e3d84bd9a8500cb09173b40520999464b
Ddbot <rakotoniaina.andry@gmail.com>
2022-10-10 10:23:04 +0200

    Created Viz presentation

--- src/screens/eventsVizScreen/Visualisation.tsx
+++ src/screens/eventsVizScreen/Visualisation.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import styled from "styled-components";
+import { icons } from '../widgetsScreen/Buttons';
 
 const hIAA = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 const categories = ['change', 'nourriture', 'medicament', 'lavage', 'dodo'];
@@ -20,13 +21,63 @@ const Text = styled.text`
     transform: translate(2.08%);
 `;
 
+const IconsGroup = styled.ul`
+    width: 80%;
+    height: 3rem;
+    margin: 0 10%;
+    padding: 0 0 .25rem 13.33%;
+
+    display: flex;
+    flex-flow: row nowrap;  
+    justify-content: space-around;
+    align-items: flex-end;
+    list-style: none;
+
+    &:nth-of-type(2){
+        align-items: flex-start;
+    }
+
+    & > li {
+        display: flex;
+        flex-flow: row nowrap;  
+        justify-content: center;
+        align-items: center;
+
+        transform: translateX(-60%);
+        border: 1px solid black;
+        border-radius: 50%;
+        & > svg {
+            scale: 0.7;
+            aspect-ratio: 1/1;
+            width: 1.6rem;
+        }
+    }
+
+        & > li:nth-of-type(2) {
+        & > svg {
+            scale: 0.6;
+            aspect-ratio: 1/1;
+            width: 1.6rem;
+        }
+    }
+`;
+
 type Props = {
 
 };
 
 const Visualisation = (props: Props) => {
-    return <SVG viewBox={`0 0 ${svgDimensions.width} ${svgDimensions.height}`}>
+    return <div>
+        <IconsGroup className='icones'>
+            {categories.map((cat, i) => { 
+                return <li key={'categorie_icone_' + cat}>
+                    { icons[cat]}
+                    </li>
+            }) }
+        </IconsGroup>  
+        <SVG viewBox={`0 0 ${svgDimensions.width} ${svgDimensions.height}`}>
         <rect x="0" y="0" width={svgDimensions.width} height={svgDimensions.height} stroke="black" fill="rgba(0,0,0,0)" strokeWidth={4} />
+        {/* lines */}
         <g className='hours'>
             {hIAA.map((h,i) => { 
                 return <g key={ 'line_hour_' + i }>
@@ -37,10 +88,20 @@ const Visualisation = (props: Props) => {
         </g>
         <g className='categories'>
             {categories.map((cat, i) => { 
-                return <line x1={(i+1) * svgDimensions.width / 6} x2={(i+1) * svgDimensions.width / 6} y1={svgDimensions.height} y2={0} strokeWidth={5} stroke="gray" />
+                return <g key={'categorie_icone_' + cat}>
+                        <line x1={(i + 1) * svgDimensions.width / 6} x2={(i + 1) * svgDimensions.width / 6} y1={svgDimensions.height} y2={0} strokeWidth={5} stroke="gray" />
+                    </g>
             }) }
-        </g>
-    </SVG>
+        </g>      
+        </SVG>
+            <IconsGroup className='icones'>
+            {categories.map((cat, i) => { 
+                return <li key={'categorie_icone2_' + cat}>
+                    { icons[cat]}
+                    </li>
+            }) }
+        </IconsGroup>  
+    </div>
 };
 
 export default Visualisation;
