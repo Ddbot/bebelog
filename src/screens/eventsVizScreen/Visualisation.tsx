ff9cfd39ff3c5aa5f3d776ced16a39f0fc3a4974
Ddbot <rakotoniaina.andry@gmail.com>
2022-10-11 22:34:06 +0200

    Cree state pour data
    data setData

--- src/screens/eventsVizScreen/Visualisation.tsx
+++ src/screens/eventsVizScreen/Visualisation.tsx
@@ -2,6 +2,12 @@ import { useState, useEffect, useRef } from 'react';
 import styled from "styled-components";
 import { icons } from '../widgetsScreen/Buttons';
 import { useLocation } from 'react-router-dom';
+import {
+    SVG,
+    Text,
+    IconsGroup,
+    VizContainer
+} from './styles';
 
 const hIAA = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 const categories = ['change', 'nourriture', 'medicament', 'lavage', 'dodo'];
@@ -11,70 +17,11 @@ const svgDimensions = {
     height: 2320
 };
 
-const SVG = styled.svg`
-    height: 88%;
-    width: 80%;
-`;
-
-const Text = styled.text`
-    font-size: 3.75rem;
-    fill: black;
-    transform: translate(2.08%);
-`;
-
-const IconsGroup = styled.ul`
-    width: 80%;
-    height: 6vh;
-    margin: 0 10%;
-    padding: 0 0 .25rem 13.33%;
-
-    display: flex;
-    flex-flow: row nowrap;  
-    justify-content: space-around;
-    align-items: flex-start;
-    list-style: none;
-
-    & > li {
-        display: flex;
-        flex-flow: row nowrap;  
-        justify-content: center;
-        align-items: center;
-
-        transform: translateX(-60%);
-        border: 1px solid black;
-        border-radius: 50%;
-        & > svg {
-            scale: 0.7;
-            aspect-ratio: 1/1;
-            width: 1.6rem;
-        }
-    }
-
-        & > li:nth-of-type(2) {
-        & > svg {
-            scale: 0.6;
-            aspect-ratio: 1/1;
-            width: 1.6rem;
-        }
-    }
-`;
-
-const VizContainer = styled.div`
-    display: flex;
-    flex-flow: column nowrap;
-    justify-content: flex-end;
-    align-items: center;
-    background: white;
-    &.blur {
-        filter: blur(10px);
-    }
-`;
-
-type Props = {
-
-};
+type Props = {};
 
 const Visualisation = (props: Props) => {
+    const [data, setData] = useState([]);
+    
     return <VizContainer className='viz'> 
         <SVG viewBox={`0 0 ${svgDimensions.width} ${svgDimensions.height}`}>
         <rect x="0" y="0" width={svgDimensions.width} height={svgDimensions.height} stroke="black" fill="rgba(0,0,0,0)" strokeWidth={4} />
