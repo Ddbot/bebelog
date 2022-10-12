33f57fbfaf5c54d1780f0caafee3c29bc1d77afb
Ddbot <rakotoniaina.andry@gmail.com>
2022-10-12 22:39:59 +0200

    Cleaning

--- src/screens/widgetsScreen/Home.tsx
+++ src/screens/widgetsScreen/Home.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import Buttons from './Buttons';
 import { HomeContainer } from './styled-components';
-import dayjs from 'dayjs';
 
 type Props = {
     handleClick: any,
