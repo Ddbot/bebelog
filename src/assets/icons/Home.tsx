6485fae3fc1973c99d81d0311da189f6b34df16e
Ddbot <rakotoniaina.andry@gmail.com>
2022-10-22 23:03:33 +0200

    Added HomeIcon in assets/icon

---
+++ src/assets/icons/Home.tsx
@@ -0,0 +1,10 @@
+const HomeIcon = (): JSX.Element => (
+  <svg viewBox="0 0 48 25" width={48} fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M47 25a.997.997 0 0 1-.691-.277L23.999 3.384 1.692 24.724a1 1 0 1 1-1.383-1.446L24 .616l23.69 22.661A1 1 0 0 1 47 25Zm-8-12a1 1 0 0 1-1-1V6h-6a1 1 0 1 1 0-2h8v8a1 1 0 0 1-1 1Z"
+      fill="#000"
+    />
+  </svg>
+)
+
+export default HomeIcon
